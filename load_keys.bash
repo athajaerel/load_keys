97b3d8cd19329d58df6cd1b1b191ad6b34af9978
Adam J. Richardson <fatman.uk@gmail.com>
2021-03-02 20:16:50 +0000

    Create predictable symlinks

--- load_keys.bash
+++ load_keys.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
-/usr/bin/ansible-playbook $(dirname $0)/load_keys.yml $*
+[ -h /tmp/ssh-agent-${USER}-screen ] || /bin/ln -s $(/bin/find /tmp/ssh-* -name agent\* -user ${USER} | /bin/head -1) /tmp/ssh-agent-${USER}-screen
+/usr/bin/ansible-playbook $(/bin/dirname $0)/load_keys.yml $*
