a0aff05b7df3ad88ab1f25e384e9a717471ae54a
Adam J. Richardson <fatman.uk@gmail.com>
2021-06-13 13:03:13 +0100

    Disable python interpreter and localhost warnings

--- load_keys.bash
+++ load_keys.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
-[ -h /tmp/ssh-agent-${USER}-screen ] || /bin/ln -s $(/bin/find /tmp/ssh-* -name agent\* -user ${USER} | /bin/head -1) /tmp/ssh-agent-${USER}-screen
-/usr/bin/ansible-playbook $(/bin/dirname $0)/load_keys.yml $*
+SSH_SOCK=/tmp/ssh-agent-${USER}-screen
+AGENT_LIST=$(/bin/find /tmp/ssh-* -name agent\* -user ${USER} | /bin/head -1)
+
+[ -h ${SSH_SOCK} ] || /bin/ln -s ${AGENT_LIST} ${SSH_SOCK}
+/usr/bin/ansible-playbook \
+	$(/bin/dirname $0)/load_keys.yml \
+	-i $(/bin/dirname $0)/inventory \
+	-e ansible_python_interpreter=auto_legacy_silent \
+	$*
