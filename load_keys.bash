be01d5640385fc880183d9c58ac1cf960340d209
Adam Richardson <adam.richardson@cgi.com>
2021-07-15 17:48:47 +0100

    Change binary paths

--- load_keys.bash
+++ load_keys.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 SSH_SOCK=/tmp/ssh-agent-${USER}-screen
-AGENT_LIST=$(/bin/find /tmp/ssh-* -name agent\* -user ${USER} | /bin/head -1)
+AGENT_LIST=$(/usr/bin/find /tmp/ssh-* -name agent\* -user ${USER} | /usr/bin/head -1)
 
 [ -h ${SSH_SOCK} ] || /bin/ln -s ${AGENT_LIST} ${SSH_SOCK}
 /usr/bin/ansible-playbook \
-	$(/bin/dirname $0)/load_keys.yml \
-	-i $(/bin/dirname $0)/inventory \
+	$(/usr/bin/dirname $0)/load_keys.yml \
+	-i $(/usr/bin/dirname $0)/inventory \
 	-e ansible_python_interpreter=auto_legacy_silent \
 	$*
