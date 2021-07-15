5da0c1c48034d79c99c145ccc45224142f34203e
Adam Richardson <adam.richardson@cgi.com>
2021-07-15 17:51:10 +0100

    Variablise script directory

--- load_keys.bash
+++ load_keys.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -euo pipefail
+
+ME_DIR=$(/usr/bin/dirname $0)
 SSH_SOCK=/tmp/ssh-agent-${USER}-screen
 AGENT_LIST=$(/usr/bin/find /tmp/ssh-* -name agent\* -user ${USER} | /usr/bin/head -1)
 
 [ -h ${SSH_SOCK} ] || /bin/ln -s ${AGENT_LIST} ${SSH_SOCK}
 /usr/bin/ansible-playbook \
-	$(/usr/bin/dirname $0)/load_keys.yml \
-	-i $(/usr/bin/dirname $0)/inventory \
+	${ME_DIR}/load_keys.yml \
+	-i ${ME_DIR}/inventory \
 	-e ansible_python_interpreter=auto_legacy_silent \
 	$*
