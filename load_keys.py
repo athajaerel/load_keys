b1be7e35244036604c75e80fff2ba14515d3898f
Adam J. Richardson <fatman.uk@gmail.com>
2022-04-15 16:00:13 +0100

    Load encrypted YAML file

--- load_keys.py
+++ load_keys.py
@@ -2,10 +2,15 @@
 
 from sys import path
 from os import environ, stat, symlink, execve
-from os.path import dirname, realpath, exists, isdir
+from os.path import dirname, realpath, exists, isdir, isfile
 from stat import S_ISSOCK
 from glob import glob
 from pwd import getpwuid
+from ansible.constants import DEFAULT_VAULT_ID_MATCH # requires ?
+from ansible.parsing.vault import VaultLib, VaultSecret # requires ?
+from getpass import getpass
+from json import loads, dumps
+from yaml import load, BaseLoader, YAMLError # requires pyyaml
 
 from config import *
 
@@ -26,6 +31,54 @@ def file_owner(filename):
 
 def issock(filename):
   return S_ISSOCK(stat(filename).st_mode)
+
+def slurp(filename):
+  blob=r''
+  with open(filename, r'rb') as f:
+    # slurp up text blob, also strip newline from end
+    blob=f.read().decode('utf-8').strip()
+    f.close()
+  return blob
+
+def isjson(myblob):
+  try:
+    loads(myblob)
+  except ValueError as e:
+    return False
+  return True
+
+def loadyaml(myblob):
+  return load(myblob, Loader=BaseLoader)
+
+def isyaml(myblob):
+  try:
+    loadyaml(myblob)
+  except YAMLError as exc:
+    print(exc)
+    return False
+  return True
+
+def objectfromyaml(myblob):
+  assert isyaml(myblob)==True
+  myyaml=loadyaml(myblob)
+  return myyaml
+
+def decryptfield(node, password):
+  # decrypt all strings beginning $ANSIBLE_VAULT;1.1;AES256
+  splitnode=node.split('\n', 1)
+  if (splitnode[0]=='$ANSIBLE_VAULT;1.1;AES256'):
+    v = VaultLib([(DEFAULT_VAULT_ID_MATCH, VaultSecret(password.encode()))])
+    v.cipher_name = 'AES256'
+    node = v.decrypt(node).decode('utf-8').strip()
+  return node
+
+def jsonwalk(node, fn, password):
+  if type(node) is dict:
+    return {k: jsonwalk(v, fn, password) for k, v in node.items()}
+  elif type(node) is list:
+    return [jsonwalk(x, fn, password) for x in node]
+  else:
+    return fn(node, password)
 
 def find_ssh_agents():
   # find all ssh agents
@@ -54,6 +107,38 @@ if my_agents==[]:
   execve(r'/usr/bin/env', [r'ssh-agent'])
   my_agents=find_ssh_agents()
 
+assert my_agents!=[]
+
+debug(ME_DIR, r'ME_DIR')
+debug(SECRET, r'SECRET')
+debug(my_agents, r'ssh agents')
+
+# get Vault password
+password=r''
+if exists(SECRET) and isfile(SECRET):
+  password=slurp(SECRET)
+else:
+  password=getpass(r'Vault password: ')
+assert password!=r''
+
+# get fully decrypted JSON object from vault
+vaultblob=slurp(r'vaults/keys.yml')
+assert vaultblob!=r''
+debug(vaultblob, 'vault blob')
+
+myobj=None
+# if it's a JSON file, load it as JSON and decrypt the password entries
+if isyaml(vaultblob):
+  myobj=objectfromyaml(vaultblob)
+  myobj=jsonwalk(myobj, decryptfield, password)
+else:
+  # otherwise, decrypt as a blob and confirm it's JSON
+  # TODO
+  vault=Vault(password)
+  myobj=vault.load(vaultblob)
+
+assert myobj!=None
+debug(myobj)
 
 #- name: 'Add keys to all owned ssh caches'
 #  vars:
@@ -64,6 +149,6 @@ if my_agents==[]:
 #    # TO DO: if no results, run ssh-agent and use that value in nest 0
 #    - name: 'Load keys into agents'
 #      with_nested:
-#        - '{{ sshAuthSock.stdout_lines }}'
+#        - '{{ sshAuthSock.files | selectattr("pw_name", "eq", lookup("env", "USER") | default("nobody")) }}'
 #        - '{{ keys }}'
 #      include_tasks: 'block_load_keys.yml'
