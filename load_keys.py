9ac9044cde109a7c94168b2af0fe463e5f913e99
Adam J. Richardson <fatman.uk@gmail.com>
2022-04-15 18:06:38 +0100

    Complete basic functionality

--- load_keys.py
+++ load_keys.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 from sys import path
-from os import environ, stat, symlink, execve
-from os.path import dirname, realpath, exists, isdir, isfile
-from stat import S_ISSOCK
+from os import environ, stat, chmod, pipe, fork, close, fdopen, dup2
+from os import open as osopen, waitpid, O_RDONLY, execve
+from os.path import realpath, isdir, exists, isfile, expanduser
 from glob import glob
 from pwd import getpwuid
-from ansible.constants import DEFAULT_VAULT_ID_MATCH # requires ?
-from ansible.parsing.vault import VaultLib, VaultSecret # requires ?
+from stat import S_ISSOCK
+from yaml import load, YAMLError, BaseLoader # requires pyyaml
+from ansible.constants import DEFAULT_VAULT_ID_MATCH
+from ansible.parsing.vault import VaultSecret, VaultLib
 from getpass import getpass
-from json import loads, dumps
-from yaml import load, BaseLoader, YAMLError # requires pyyaml
 
 from config import *
 
@@ -18,6 +18,9 @@ USER=environ.get(r'USER')
 ME_DIR=path[0]
 SECRET=realpath(r'%s/vaults/secret.txt' % ME_DIR)
 WISP_PATH=r'/dev/shm/wisp.bash'
+BIN_ENV=r'/usr/bin/env'
+BIN_AGENT=r'/usr/bin/ssh-agent'
+BIN_ADD=r'/usr/bin/ssh-add'
 
 def debug(line, prefix=None):
   if (DEBUG_MODE):
@@ -40,13 +43,6 @@ def slurp(filename):
     f.close()
   return blob
 
-def isjson(myblob):
-  try:
-    loads(myblob)
-  except ValueError as e:
-    return False
-  return True
-
 def loadyaml(myblob):
   return load(myblob, Loader=BaseLoader)
 
@@ -59,7 +55,7 @@ def isyaml(myblob):
   return True
 
 def objectfromyaml(myblob):
-  assert isyaml(myblob)==True
+  assert isyaml(myblob)==True, r'Not well-formed YAML'
   myyaml=loadyaml(myblob)
   return myyaml
 
@@ -67,7 +63,8 @@ def decryptfield(node, password):
   # decrypt all strings beginning $ANSIBLE_VAULT;1.1;AES256
   splitnode=node.split('\n', 1)
   if (splitnode[0]=='$ANSIBLE_VAULT;1.1;AES256'):
-    v = VaultLib([(DEFAULT_VAULT_ID_MATCH, VaultSecret(password.encode()))])
+    a = [(DEFAULT_VAULT_ID_MATCH, VaultSecret(password.encode()))]
+    v = VaultLib(a)
     v.cipher_name = 'AES256'
     node = v.decrypt(node).decode('utf-8').strip()
   return node
@@ -102,12 +99,34 @@ def find_ssh_agents():
       my_agents+=[agent_file]
   return my_agents
 
+def parentpostfork(rside, wside):
+  close(wside)
+  for line in fdopen(rside):
+    print(line.strip())
+  # reap zombie
+  pid, status=waitpid(-1, 0)
+  print('Child exited: pid %d returned %d' % (pid, status))
+
+def childpostfork(rside, wside):
+  close(rside)
+  dup2(wside, 1) # Redirect stdout to parent
+  dup2(wside, 2) # Redirect stderr to parent
+  devnull=osopen('/dev/null', O_RDONLY)
+  dup2(devnull, 0)
+
 my_agents=find_ssh_agents()
 if my_agents==[]:
-  execve(r'/usr/bin/env', [r'ssh-agent'])
+  rside, wside = pipe()
+  if not fork():
+    childpostfork(rside, wside);
+    # Execute the desired program, replace the program image,
+    # doesn't return
+    execve(BIN_ENV, [BIN_ENV, BIN_AGENT], environ)
+    raise ValueError('Failed to exec ssh-agent')
+  parentpostfork(rside, wside)
   my_agents=find_ssh_agents()
 
-assert my_agents!=[]
+assert my_agents!=[], r'Agent could not be started.'
 
 debug(ME_DIR, r'ME_DIR')
 debug(SECRET, r'SECRET')
@@ -119,15 +138,15 @@ if exists(SECRET) and isfile(SECRET):
   password=slurp(SECRET)
 else:
   password=getpass(r'Vault password: ')
-assert password!=r''
+assert password!=r'', r'Empty password entered.'
 
 # get fully decrypted JSON object from vault
 vaultblob=slurp(r'vaults/keys.yml')
-assert vaultblob!=r''
+assert vaultblob!=r'', r'Empty vault blob read.'
 debug(vaultblob, 'vault blob')
 
 myobj=None
-# if it's a JSON file, load it as JSON and decrypt the password entries
+# if it's a JSON file, load it as JSON and decrypt the passwords
 if isyaml(vaultblob):
   myobj=objectfromyaml(vaultblob)
   myobj=jsonwalk(myobj, decryptfield, password)
@@ -137,18 +156,33 @@ else:
   vault=Vault(password)
   myobj=vault.load(vaultblob)
 
-assert myobj!=None
-debug(myobj)
+assert myobj!=None, r'Nothing returned from vault decryption'
 
-#- name: 'Add keys to all owned ssh caches'
-#  vars:
-#    wisp_full_path: '/dev/shm/wisp.bash'
-#  vars_files:
-#    - 'vaults/keys.yml'
-#
-#    # TO DO: if no results, run ssh-agent and use that value in nest 0
-#    - name: 'Load keys into agents'
-#      with_nested:
-#        - '{{ sshAuthSock.files | selectattr("pw_name", "eq", lookup("env", "USER") | default("nobody")) }}'
-#        - '{{ keys }}'
-#      include_tasks: 'block_load_keys.yml'
+# TODO: assert mounted location is executable
+
+for key in myobj[0]['keys']:
+  debug(key, 'Key')
+  debug(my_agents, r'ssh agents')
+  # expand key['path'], ssh-add doesn't like tildes
+  key['path']=expanduser(key['path'])
+  for agent in my_agents:
+    # create wisp script
+    with open(WISP_PATH, r'w') as f:
+      f.write((r"""#!%s bash
+echo '%s'
+/bin/rm %s""" % (BIN_ENV, key['password'], WISP_PATH)))
+      f.close()
+    chmod(WISP_PATH, 0o755)
+    # add key to agent
+    rside, wside = pipe()
+    environ.update({ 'SSH_AUTH_SOCK': agent })
+    environ.update({ 'SSH_ASKPASS': WISP_PATH })
+    environ.update({ 'DISPLAY': '' })
+    if not fork():
+      childpostfork(rside, wside);
+      # Execute the desired program, replace the program image,
+      # doesn't return
+      execve(BIN_ADD, [BIN_ADD, key['path']], environ)
+      raise ValueError('Failed to exec ssh-agent')
+    parentpostfork(rside, wside)
+...
